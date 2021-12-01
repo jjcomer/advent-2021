0fbf0b46987fd3a07c9f9034b046cdce579ca933
josh <jcomer@cvent.com>
2021-12-01 05:24:54 -0400

    Solve Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,9 +1,35 @@
 #[aoc_generator(day1)]
 pub fn parse_input(input: &str) -> Vec<i32> {
-    Vec::new()
+    input.lines().map(|s| s.parse::<i32>().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &Vec<i32>) -> i32 {
-    0
+    let mut counter = 0;
+    let mut last = &i32::MAX;
+
+    for i in input.iter() {
+        if i > last {
+            counter += 1;
+        }
+        last = i;
+    }
+
+    counter
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &Vec<i32>) -> i32 {
+    let mut counter = 0;
+    let mut last = i32::MAX;
+
+    for x in input.windows(3) {
+        let current = x.iter().sum();
+        if current > last {
+            counter += 1;
+        }
+        last = current
+    }
+
+    counter
 }

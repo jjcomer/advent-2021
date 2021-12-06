88743ea496ef918509e5e17189ab69032ff97b21
josh <jcomer@cvent.com>
2021-12-06 05:51:43 -0400

    Solve day 06

---
+++ src/day06.rs
@@ -0,0 +1,30 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day6)]
+pub fn parse_input(input: &str) -> Vec<usize> {
+    let mut fish = vec![0; 9];
+    for n in input.split(",").map(|n| n.parse::<usize>().unwrap()) {
+        fish[n] = fish[n] + 1
+    }
+    fish
+}
+
+fn reproduce(input: &Vec<usize>, days: usize) -> usize {
+    let mut fish = input.to_owned();
+    for _ in 0..days {
+        let new_fish = fish.remove(0);
+        fish.push(new_fish);
+        fish[6] = fish[6] + new_fish;
+    }
+    fish.iter().sum()
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input: &Vec<usize>) -> usize {
+    reproduce(input, 80)
+}
+
+#[aoc(day6, part2)]
+pub fn solve_part2(input: &Vec<usize>) -> usize {
+    reproduce(input, 256)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 
 aoc_runner_derive::aoc_lib! { year = 2021 }
